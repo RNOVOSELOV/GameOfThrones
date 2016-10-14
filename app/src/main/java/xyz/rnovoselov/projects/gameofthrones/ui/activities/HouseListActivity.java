6003522e6f6b2a8ef6eb8fde1adee4de837996f2
Роман Новоселов <roman@MacBook-Pro-Roman.local>
2016-10-15 00:33:34 +0300

    Активити список добавил дровер

--- app/src/main/java/xyz/rnovoselov/projects/gameofthrones/ui/activities/HouseListActivity.java
+++ app/src/main/java/xyz/rnovoselov/projects/gameofthrones/ui/activities/HouseListActivity.java
@@ -1,19 +1,25 @@
 package xyz.rnovoselov.projects.gameofthrones.ui.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -41,6 +47,11 @@ public class HouseListActivity extends BaseActivity {
     TabLayout mTabLayout;
     @BindView(R.id.house_activity_view_pager)
     ViewPager mViewPager;
+    @BindView(R.id.house_activity_drawer)
+    DrawerLayout mNavigationDrawer;
+    @BindView(R.id.house_activity_navigation_view)
+    NavigationView mNavigationView;
+
     private int[] tabIcons = {
             R.drawable.lanister_icon,
             R.drawable.stark_icon,
@@ -60,9 +71,12 @@ public class HouseListActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(mToolbar);
-//        if (getSupportActionBar() != null) {
-//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        setupDrawer();
 
         setupViewPager(mViewPager);
         mTabLayout.setupWithViewPager(mViewPager);
@@ -71,12 +85,72 @@ public class HouseListActivity extends BaseActivity {
         setupTabIcon(ConstantManager.TARGARYENS_HOUSE_TAB_ID);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mNavigationDrawer.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mNavigationDrawer.isDrawerOpen(GravityCompat.START)) {
+            mNavigationDrawer.closeDrawer(GravityCompat.START);
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    private void setupDrawer() {
+        mNavigationView.setItemIconTintList(null);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.drawer_menu_lannisters_item:
+                        mViewPager.setCurrentItem(0, true);
+                        break;
+                    case R.id.drawer_menu_starks_item:
+                        mViewPager.setCurrentItem(1, true);
+                        break;
+                    case R.id.drawer_menu_targariens_item:
+                        mViewPager.setCurrentItem(2, true);
+                        break;
+                }
+                item.setChecked(true);
+                mNavigationDrawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
     private void setupViewPager(ViewPager vp) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(HouseFragment.newInstance(AppConfig.LANNISTER_HOUSE_ID), getResources().getString(R.string.lannister_home_title));
         adapter.addFragment(HouseFragment.newInstance(AppConfig.STARK_HOUSE_ID), getString(R.string.stark_home_title));
         adapter.addFragment(HouseFragment.newInstance(AppConfig.TARGARYEN_HOUSE_ID), getString(R.string.targariens_home_title));
         mViewPager.setAdapter(adapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mNavigationView.getMenu().getItem(0).setChecked(false);
+                mNavigationView.getMenu().getItem(1).setChecked(false);
+                mNavigationView.getMenu().getItem(2).setChecked(false);
+                mNavigationView.getMenu().getItem(position).setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     private void setupTabIcon(int tabAtId) {
